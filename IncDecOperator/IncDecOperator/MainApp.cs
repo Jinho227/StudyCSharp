976d63efde3b1fbd73f1b7e0e47e831fd115dab6
Jinho227 <sreika227@gmail.com>
2022-09-01 18:25:57 +0900

    first commit
    commit StudyCSharp for the first time

---
+++ IncDecOperator/IncDecOperator/MainApp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace IncDecOperator
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Console.WriteLine(a++);
+            Console.WriteLine(++a);
+            Console.WriteLine(a--);
+            Console.WriteLine(--a);
+        }
+    }
+}
